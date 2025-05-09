d522e256870eaf46ee4fd940c2bba38466a0a24e
Sir-Umair <umairsahib242@gmail.com>
2025-05-09 23:01:11 +0500

    first commit

--- arrays.py
+++ arrays.py
@@ -1,6 +1,22 @@
-array=[1,3,5,7,9,11]
-print(array[0:3])
-a=[10]
-a=[[6,9,12],
-   [77,89,89]]
-print(a[0][2])
+# array=[1,3,5,7,9,11]
+# print(array[0:3])
+# a=[10]
+# a=[[6,9,12],
+#    [77,89,89]]
+# print(a[0][2])
+# 2d array
+array_2d=[[1,2,3],
+           [4,5,6],
+           [7,8,9]]
+print(array_2d[0:2]
+)
+print(array_2d[0][1])
+print(array_2d[1][2])
+for i in array_2d:
+    print(i)
+# 3d array
+array_3d=[[[1,2,3],
+             [4,5,6]],
+            [[7,8,9],
+             [10,11,12]]]
+print(array_3d[0][1][2])    
